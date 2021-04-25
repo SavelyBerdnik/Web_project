2e2716df5b6d909df6d81fed2ad545980415b38f
Savely <74881622+SavelyBerdnik@users.noreply.github.com>
2021-04-25 23:14:49 +0200

    Add files via upload

---
+++ WEB_Project/data/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
+
+
+class InputForm(FlaskForm):
+    code = StringField('Введите код', validators=[DataRequired()])
+    submit = SubmitField('Перейти')
--- WEB_Project/main.py
+++ WEB_Project/main.py
@@ -1,11 +1,9 @@
 from flask_restful import reqparse, abort, Api, Resource
-from flask import Flask, jsonify, render_template, redirect
+from flask import Flask, request, jsonify, render_template, redirect
 from data import news_resources, db_session
 from data.users import User
 import json
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from data.forms import *
 
 app = Flask(__name__)
 
@@ -13,25 +11,6 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 api = Api(app)
 db_session.global_init("db/main.db")
 session = db_session.create_session()
-
-
-class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
-
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    submit = SubmitField('Зарегистрироваться')
-
-
-class InputForm(FlaskForm):
-    code = StringField('Введите код', validators=[DataRequired()])
-    submit = SubmitField('Перейти')
 
 
 @app.route('/')
@@ -42,7 +21,7 @@ def login():
         if session.query(User).filter(User.email == form.email and
                                       User.login == form.username and
                                       User.password == form.password):
-            return redirect('/create')
+            return redirect('/main_page')
     return render_template('login.html', title='Авторизация', form=form)
 
 
@@ -70,14 +49,23 @@ def code_input():
     return render_template('code_input.html', title='Ввод кода', form=form)
 
 
+@app.route('/page_name')
+
+
 @app.route('/quest')
 def quest():
     return render_template('quest.html')
 
 
-@app.route('/create')
-def create():
-    return render_template('create.html')
+@app.route('/main_page')
+def main_page():
+    if request.method == 'POST':
+        select = request.form.get('method_cipher')
+        if select == 'open_form':
+            pass
+        else:
+            return redirect('/create_quest')
+    return render_template('main_page.html')
 
 
 if __name__ == '__main__':
